e29b52c2a73b202e7c28b8b8916049149befce19
Juan von Doom <phazhion@gmail.com>
2019-08-18 09:15:46 -0600

    Created footer component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
 import React from "react"
 
 import Header from "./header"
+import Footer from "./footer"
 
 export default ({ children }) => (
   <div className="container mx-auto">
     <Header />
     <main className="mb-4">{children}</main>
+    <Footer />
   </div>
 )
