64bbd79dc8c6225355543a9dad1b26b1bab65258
Juan von Doom <phazhion@gmail.com>
2019-09-02 17:10:03 -0600

    Created About page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const About = props => {
+  return (
+    <Layout>
+      <p>About</p>
+    </Layout>
+  )
+}
+
+export default About
